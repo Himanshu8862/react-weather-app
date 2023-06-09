81dd988e6c1c34784bc2745e5836b8f908b19b84
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-09 17:37:52 +0530

    search component

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const GeoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const GeoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '834a4675d4mshd20fd4989eb60d7p1f6fbdjsn24c7efbd3307',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
