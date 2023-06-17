1a20a829b8b3ca20eb532b331d898671dded6427
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-17 11:54:23 +0530

    fixed bugs

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ export const GeoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const GeoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '834a4675d4mshd20fd4989eb60d7p1f6fbdjsn24c7efbd3307',
+		'X-RapidAPI-Key': window.env.REACT_APP_GEODB_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const WeatherApiUrl = "https://api.openweathermap.org/data/2.5"
-export const WeatherApiKey = "aea93c16ad3ab83a2e1a0baa686bdfd7"
+export const WeatherApiKey = window.env.REACT_APP_OPEN_WEATHER_API_KEY
